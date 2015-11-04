d10b04b330b49f116b0fd3acfb8770bc25b34ce7
= <jfuentes@zeek.cl>
2015-11-04 03:31:27 -0300

    test code

---
+++ src/test/groovy/labmmba/Sciencist_publicationSpec.groovy
@@ -0,0 +1,22 @@
+package labmmba
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(Sciencist_publication)
+class Sciencist_publicationSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
