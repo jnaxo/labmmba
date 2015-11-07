b31c50aeaf0ca73560f4d2c6c83572ec6a90f6bc
= <jfuentes@zeek.cl>
2015-11-07 19:56:12 -0300

    compilation update

---
+++ grails-app/conf/application.groovy
@@ -0,0 +1,23 @@
+
+grails.plugin.springsecurity.successHandler.alwaysUseDefault = true
+grails.plugin.springsecurity.auth.loginFormUrl = '/user/login'
+grails.plugin.springsecurity.successHandler.defaultTargetUrl = '/user/index'
+
+// Added by the Spring Security Core plugin:
+grails.plugin.springsecurity.userLookup.userDomainClassName = 'labmmba.User'
+grails.plugin.springsecurity.userLookup.authorityJoinClassName = 'labmmba.UserRole'
+grails.plugin.springsecurity.authority.className = 'labmmba.Role'
+grails.plugin.springsecurity.controllerAnnotations.staticRules = [
+	'/':                ['permitAll'],
+	'/error':           ['permitAll'],
+	'/index':           ['permitAll'],
+	'/index.gsp':       ['permitAll'],
+	'/shutdown':        ['permitAll'],
+	'/assets/**':       ['permitAll'],
+	'/**/js/**':        ['permitAll'],
+	'/**/css/**':       ['permitAll'],
+	'/**/images/**':    ['permitAll'],
+	'/**/favicon.ico':  ['permitAll'],
+	'/user/index':		['permitAll'],
+	'/user/login':		['permitAll']
+]
